66dfa03eef5707c3fd9d1208339954234a97da57
Aleksandr Kononenko <sawa2375@gmail.com>
2021-11-09 20:08:43 +0200

    add filter by category, refactor

--- motocycle/motocycleView.js
+++ motocycle/motocycleView.js
@@ -13,26 +13,5 @@ export default class MotocycleView extends View {
     render(data) {
         const listHTML = data.map((el) => cardRender(el));
         this.insertHTML(listHTML, this.dom.cartBox);
-
     }
-
-    // render = (data) => {
-    //     const listHTML = data.map((el) => {
-    //         return `
-    //         <div class="card" style="width: 15rem;">
-    //         <div class="card-img">
-    //             <img src=${el.Image} class="card-img-top" alt="...">
-    //             <p class="card-price">$${el.Price}</p>
-    //         </div>
-    //         <div class="card-body">
-    //           <h5 class="card-title">${el.Brand} ${el.Model}</h5>
-    //           <p class="type">${el['Type of moto']}</p>
-    //           <p class="card-text">${this.shortDescription(el.Description)}</p>
-    //           <a href="#" class="btn btn-primary">Open</a>
-    //         </div>
-    //       </div>
-    //     `;
-    //     });
-    //     this.insertHTML(listHTML, this.dom.cartBox);
-    // };
 }
