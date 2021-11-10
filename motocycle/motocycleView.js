832e7279a55fb1e15143d7991aa96522d2225438
Maria358 <mlysenko358@gmail.com>
2021-11-10 18:40:27 +0300

    add pagination

--- motocycle/motocycleView.js
+++ motocycle/motocycleView.js
@@ -4,18 +4,19 @@ import cardRender from '../common/cardRender.js';
 export default class MotocycleView extends View {
     dom = {
         cartBox: document.querySelector('.cart-box'),
-        openBtns: document.querySelectorAll('.open')
+        openBtns: document.querySelectorAll('.open'),
+        pagination: document.querySelector('.pagination')
     };
 
     constructor(onOpen) {
         super();
         this.onOpen = onOpen;
     }
-  
+
     render(data) {
         const listHTML = data.map((el) => cardRender(el));
         this.insertHTML(listHTML, this.dom.cartBox);
-           document.querySelectorAll('.open').forEach((btn) =>
+        document.querySelectorAll('.open').forEach((btn) =>
             btn.addEventListener('click', (event) => {
                 const moto = event.path[1].querySelector('.card-title').textContent;
                 this.onOpen(moto);
