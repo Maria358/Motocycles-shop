14fc24c76bb7177d991eb98b0505ccfce33645dc
Aleksandr Kononenko <sawa2375@gmail.com>
2021-11-10 11:27:26 +0200

    add footer

--- motocycle/motocycleView.js
+++ motocycle/motocycleView.js
@@ -6,13 +6,15 @@ export default class MotocycleView extends View {
         cartBox: document.querySelector('.cart-box'),
         sortBy: document.querySelectorAll('.sortby'),
         sortVal: document.querySelector('.sortVal'),
-        openBtns: document.querySelectorAll('.open')
+        openBtns: document.querySelectorAll('.open'),
+        paginationItems: document.querySelectorAll('.pagination .page-link')
     };
 
     constructor(onSelectSort, onOpen) {
         super();
         this.onOpen = onOpen;
         this.giveAction(this.dom.sortBy, this.dom.sortVal, onSelectSort);
+        this.pagination(this.dom.paginationItems)
     };
 
     giveAction(collection, element, handler) {
@@ -32,12 +34,26 @@ export default class MotocycleView extends View {
         cards.forEach(card => card.style.display = 'none');
     };
 
+    pagination = (data, selector) => {
+        const notesOnPage = 6;
+        for (const el of selector) {
+            el.addEventListener('click', () => {
+                let pageNum = +el.textContent
+                let start = (pageNum - 1) * notesOnPage
+                let end = start + notesOnPage;
+                let notes = data.slice(start, end)
+                console.log(notes)
+            })
+        }
+    }
+
+
     render(data) {
         const listHTML = data.map((el) => cardRender(el));
         this.insertHTML(listHTML, this.dom.cartBox);
         document.querySelectorAll('.open').forEach(btn => btn.addEventListener('click', (event) => {
             const moto = event.path[1].querySelector('.card-title').textContent;
             this.onOpen(moto);
-        }))
+        }))        
     }
 }
